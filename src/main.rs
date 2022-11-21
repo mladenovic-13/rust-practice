f9d14d0bde816c8aebfe6e3b50c3e180e2d29fd0
mladenovic13 <nikolanik999@gmail.com>
2022-11-22 00:53:04 +0100

    Main concepts test

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,94 @@
-fn main() {}
+use std::io;
+
+#[derive(Debug)]
+struct Student {
+    name: String,
+    lastname: String,
+    grades: Vec<u32>,
+}
+// impl Student {
+//     fn get_final_grade(&self) -> f32 {
+//         let mut sum: f32 = 0.0;
+//         for grade in &self.grades {
+//             sum += *grade as f32;
+//         }
+//         sum / self.grades.len() as f32
+//     }
+// }
+
+fn main() {
+    println!("Welcome to students service!");
+
+    let mut students: Vec<Student> = Vec::new();
+    let mut students_count = String::new();
+    println!("How much students you want to enter?");
+    io::stdin()
+        .read_line(&mut students_count)
+        .expect("Failed to read the line!");
+
+    let students_count: u32 = match students_count.trim().parse() {
+        Ok(input) => input,
+        Err(_) => {
+            println!("You must enter a valid number!");
+            std::process::exit(1)
+        }
+    };
+
+    let mut i = 0;
+    while i < students_count {
+        let mut name = String::new();
+        let mut lastname = String::new();
+        let mut grades: Vec<u32> = Vec::new();
+        let mut grades_count = String::new();
+
+        println!("Please enter student's name: ");
+        io::stdin()
+            .read_line(&mut name)
+            .expect("Failed to read the line!");
+
+        println!("Please enter student's lastname: ");
+        io::stdin()
+            .read_line(&mut lastname)
+            .expect("Failed to read the line!");
+
+        println!("How much grades you want to enter?");
+        io::stdin()
+            .read_line(&mut grades_count)
+            .expect("Failed to read the line!");
+
+        let grades_count: u32 = match grades_count.trim().parse() {
+            Ok(count) => count,
+            Err(_) => {
+                println!("You must enter valid number!");
+                break;
+            }
+        };
+
+        for i in 0..grades_count {
+            println!("Enter {}. grade:", i + 1);
+            let mut grade = String::new();
+            io::stdin()
+                .read_line(&mut grade)
+                .expect("Failed to read the line!");
+            match grade.trim().parse() {
+                Ok(input) => grades.push(input),
+                Err(_) => {
+                    println!("You must enter valid number!");
+                    break;
+                }
+            };
+        }
+
+        let student = Student {
+            name,
+            lastname,
+            grades,
+        };
+
+        students.push(student);
+        // Loop counter
+        i += 1;
+    }
+
+    println!("{:#?}", students);
+}
